9bd4d621d5912f8a917ba156a2c294a48d470b92
GreenRamSilgen <ps3iswow@gmail.com>
2020-06-25 18:12:50 -0700

    js dom practice

--- JavascriptBasics/main.js
+++ JavascriptBasics/main.js
@@ -1,4 +1,29 @@
-let mydate = "19" + "67";
+const body = document.querySelector('body');
 
-console.log(mydate);
-console.log(typeof mydate);
+//red <p>text</p> 
+const myP = document.createElement('p');
+myP.textContent = "Hey I'm red!";
+myP.style.color = 'red';
+body.appendChild(myP);
+
+//blue <h3>text</h3>
+const blueH3 = document.createElement('h3');
+blueH3.textContent = "I'm a blue h3!";
+blueH3.style.color = 'Blue';
+body.appendChild(blueH3);
+
+//black border, pink background div
+const mainDiv = document.createElement('div');
+
+//add h1 and p 
+
+const myH1 = document.createElement('h1');
+myH1.textContent = "I'm in a div";
+
+const myDivP = document.createElement('p');
+myDivP.textContent = "ME TOO!";
+
+mainDiv.appendChild(myH1);
+mainDiv.appendChild(myDivP);
+
+body.appendChild(mainDiv);
