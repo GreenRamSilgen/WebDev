398c58c2becd1e19e6616ba092b4e9a51749fb44
GreenRamSilgen <ps3iswow@gmail.com>
2020-06-29 15:26:01 -0700

    palindrome exercise complete

--- javascript-exercises-master/palindromes/palindromes.js
+++ javascript-exercises-master/palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function() {
-
-}
+const palindromes = function(input) {
+    input = input.toLowerCase();
+    input = input.replace(/ |\.|,|!/g,"");
+    let left =0;
+    let right = input.length-1;
+    let bool = true;
+    while(left <= right)
+    {
+        if(input.charAt(left) === input.charAt(right))
+        {
+            left++;
+            right--;
+        }
+        else{
+            return false;
+        }
+    }
+    return true;
+ }
 
 module.exports = palindromes
