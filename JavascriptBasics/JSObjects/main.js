227d4e6ee4a3129ecff86069be7baf53b093d92d
GreenRamSilgen <ps3iswow@gmail.com>
2020-07-03 18:03:26 -0700

    basic oop library card

---
+++ JavascriptBasics/JSObjects/main.js
@@ -0,0 +1,43 @@
+const display = document.querySelector("#card-holder");
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function () {
+        return this.title + " by " + this.author + ", " + this.pages + ", " + (this.read ? "read it." : "not read yet.");
+    };
+}
+
+
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+function render() {
+    for (let i = 0; i < myLibrary.length; i++) {
+        let card = document.createElement("div");
+        card.setAttribute("class", "card");
+        let title = makeElement("h2", myLibrary[i].title);
+        title.setAttribute("class", "card-title");
+        let author = makeElement("h3", myLibrary[i].author);
+        author.setAttribute("class", "class-author");
+
+        //adding elements to card
+        card.appendChild(title);
+        card.appendChild(author);
+
+        //add card to page
+        display.appendChild(card);
+    }
+
+}
+
+function makeElement(elementType, content) {
+    let Element = document.createElement(elementType);
+    Element.textContent = content;
+    return Element;
+}
