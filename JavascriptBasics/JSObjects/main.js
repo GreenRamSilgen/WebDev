bdf8125971aa0f938f29c621eb7caaf8a90e30b6
GreenRamSilgen <ps3iswow@gmail.com>
2020-07-04 19:12:45 -0700

    completed library

--- JavascriptBasics/JSObjects/main.js
+++ JavascriptBasics/JSObjects/main.js
@@ -1,39 +1,76 @@
-const display = document.querySelector("#card-holder");
-const popUpDisplay = document.querySelector(".bg-modal");
+const cardHolder = document.querySelector("#cardHolder");
 let popped = false;
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
+//BOOK Object constructor
+function Book(title="No Title", author="No Author", pages=0, read=false) {
+    
+    this.title = (title == "" ? "No Title" : title);
+    this.author = (author == "" ? "No Author" : author);
+    this.pages = (pages == "" ? "No Pages" : pages);
     this.read = read;
     this.info = function () {
-        return this.title + " by " + this.author + ", " + this.pages + ", " + (this.read ? "read it." : "not read yet.");
+        return (this.read ? "Read." : "Not Read");
     };
 }
 
 
-
+//Adds book to library/ Array
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
+//create cards for all books in page
 function render() {
+    cardHolder.innerHTML="";
     for (let i = 0; i < myLibrary.length; i++) {
+        //card element
         let card = document.createElement("div");
         card.setAttribute("class", "card");
+        
+        //title
         let title = makeElement("h2", myLibrary[i].title);
-        title.setAttribute("class", "card-title");
+        title.setAttribute("class", "card__content");
+        //author
         let author = makeElement("h3", myLibrary[i].author);
-        author.setAttribute("class", "class-author");
+        author.setAttribute("class", "card__content");
+        //pages
+        let page = makeElement("h3", myLibrary[i].pages);
+        title.setAttribute("class", "card__content");
+        //read or not
+        let boolRead = makeElement("h3", myLibrary[i].info());
+        author.setAttribute("class", "card__content");
+        //removev button
+        let removeBtn = makeElement("button", "Remove");
+        removeBtn.setAttribute("class","card__btn newCardBtn");
+        removeBtn.setAttribute("id","rmvCard");
+        removeBtn.addEventListener('click', function(){myLibrary.splice(i,1); render()});
 
-        //adding elements to card
+        //read on or off
+        let readBtn = makeElement("button","Read");
+        readBtn.setAttribute("class","card__btn newCardBtn");
+        readBtn.setAttribute("id", "toggleRead");
+        readBtn.addEventListener('click',function(){
+            if(myLibrary[i].read == true)
+            {
+                myLibrary[i].read = false;
+            }
+            else{
+                myLibrary[i].read = true;
+            }
+            render();
+        });
+
+
+        //adding book info to card
         card.appendChild(title);
         card.appendChild(author);
-
+        card.appendChild(page);
+        card.appendChild(boolRead);
+        card.appendChild(removeBtn);
+        card.appendChild(readBtn);
         //add card to page
-        display.appendChild(card);
+        cardHolder.appendChild(card);
     }
 
 }
@@ -45,7 +82,17 @@ function makeElement(elementType, content) {
 }
 
 
-//POP UP CONTROLS
+//Modal Section
+const popUpDisplay = document.querySelector(".bg-modal");
+
+//submit button and function on click
+let userIn = document.querySelector("#userInput");
+userIn.onsubmit = function(e){
+    e.preventDefault();
+    addBookToLibrary(new Book(userIn.title.value,userIn.author.value, userIn.pages.value, userIn.read.checked));
+    render();
+}
+
 function popUp(){
     if(popped == true)
     {
@@ -57,3 +104,6 @@ function popUp(){
         popped = true;
     }
 }
+
+
+//Modal Section
